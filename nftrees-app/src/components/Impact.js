0ca78382550fe5482676ef0a7e8ae92d96ef2e2a
niconeven <nneven@usc.edu>
2021-08-19 15:37:06 -0700

    update impact page

--- nftrees-app/src/components/Impact.js
+++ nftrees-app/src/components/Impact.js
@@ -3,43 +3,37 @@ import React, { PureComponent , useState, useEffect, useImperativeHandle} from '
 import './Impact.css';
 import { calculateAddressEmissions } from "ethereum-emissions-calculator";
 import CountUp from 'react-countup';
+import firebase from '../firebase';
 
 function Impact(props) {
   const[totalGas, setTotalGas] = useState(0);
     const[totalKg, setTotalKg] = useState(0);
     const[totalTransactions, setTotalTransactions] = useState(0);
-    const[validInput, setValidInput] = useState();
-    const[calculating, setCalculating] = useState(false);
-    var loading;
 
     useEffect(() => {
-        /*const calculate = async () => {
-          await handleCalculateEmissions();
-          console.log(totalTransactions);
-        }
-        calculate();
-        loading = document.querySelector('#loading');*/
         handleCalculateEmissions();
-    }, [calculating, validInput]);
+    }, []);
 
 
     const handleCalculateEmissions = async () => {
-        setCalculating(true);
         setTotalGas(undefined);
         setTotalKg(undefined);
         setTotalTransactions(undefined);
 
-        //loading.style.display = 'flex';
         var gas = 0;
         var co2 = 0;
         var transactions = 0;
         var typeTransaction = ['eth', 'erc20', 'erc721'];
+        var apiKey;
+        const address = props.account;
+        const callableReturnMessage = firebase.functions().httpsCallable('getEtherscanKey');
+    
+        await callableReturnMessage().then((result) => {
+            apiKey = result.data.etherscanKey;
+        }).catch((error) => {
+          console.log(`error: ${JSON.stringify(error)}`);
+        });
 
-        //const address = props.account;
-        const address = '0xBB379331De54A7c0a4b2bfF5A54A14cdba7E9E6d';
-        console.log(address);
-        //const apiKey = process.env.REACT_APP_ETHERSCAN_API_KEY;
-        const apiKey = 'NDKJ13HJPXBGN9AD9DQKJH25W1FDC86IDR';
         for (var i = 0; i < 3; i++) {
             const emissions = await calculateAddressEmissions({
                 transactionType: typeTransaction[i],
@@ -55,9 +49,6 @@ function Impact(props) {
         setTotalGas(gas);
         setTotalKg(co2);
         setTotalTransactions(transactions);
-        
-        //loading.style.display = 'none';
-        setCalculating(false);
     }
   
   return (
@@ -67,7 +58,8 @@ function Impact(props) {
           <div className = 'dashboardHeader'>
             <p className = 'dashboardTitle'>IMPACT DASHBOARD</p>
             <p className = 'carbonFootprint'>CARBON FOOTPRINT ⓘ</p>
-            <p className = 'carbonFootprintValue'>1XXXXX kg CO2</p>
+            <p className = 'carbonFootprintValue'><CountUp className = 'carbonFootprintValue' end = {totalKg} duration = {1} separator ={','}/> kg CO2</p>
+            
           </div>
           <p className = 'emissionsTitle'>EMISSIONS</p>
           <div className = 'dashboardContent'>
@@ -76,12 +68,12 @@ function Impact(props) {
               <CountUp className = 'emissionsValue' end = {totalTransactions} duration = {1} separator ={','}/>
             </div>
             <div className = 'dashboardMiddle'>
-              <p className = 'contentHeader'>Gas spent</p>
-              <CountUp className = 'emissionsValue' end = {totalTransactions} duration = {1} separator ={','}/>
+              <p className = 'contentHeader'>Gas spent (wei)</p>
+              <CountUp className = 'emissionsValue' end = {totalGas} duration = {1} separator ={','}/>
             </div>
             <div className = 'dashboardRight'>
-              <p className = 'contentHeader'>CO2 produced</p>
-              <CountUp className = 'emissionsValue' end = {totalTransactions} duration = {1} separator ={','}/>
+              <p className = 'contentHeader'>CO2 produced (kg)</p>
+              <CountUp className = 'emissionsValue' end = {totalKg} duration = {1} separator ={','}/>
             </div>
           </div>
           <p className = 'impactTitle'>IMPACT</p>
@@ -90,12 +82,12 @@ function Impact(props) {
               <p className = 'contentHeader'>NFTrees</p>
               <CountUp className = 'impactValue' end = {totalTransactions} duration = {1} separator ={','}/>
             </div>
-            <div className = 'dashboardMiddle'>
-              <p className = 'contentHeader'>CO2 offset</p>
-              <CountUp className = 'impactValue' end = {totalTransactions} duration = {1} separator ={','}/>
-            </div>
             <div className = 'dashboardRight'>
               <p className = 'contentHeader'>Trees planted</p>
+              <CountUp className = 'impactValue' end = {totalTransactions} duration = {1} separator ={','}/>
+            </div>
+            <div className = 'dashboardMiddle'>
+              <p className = 'contentHeader'>CO2 offset</p>
               <CountUp className = 'impactValue' end = {totalTransactions} duration = {1} separator ={','}/>
             </div>
           </div>
