dd24d2fef514e257af6f4f75ee394e062f9dc3f6
Brendan <b.k.morrison2@gmail.com>
2021-08-24 00:17:28 -0700

    class restructure :DD

--- nftrees-app/src/components/Impact.js
+++ nftrees-app/src/components/Impact.js
@@ -5,109 +5,109 @@ import { calculateAddressEmissions } from "ethereum-emissions-calculator";
 import CountUp from 'react-countup';
 import firebase from '../firebase';
 
-function Impact(props) {
-  const[totalGas, setTotalGas] = useState(0);
-    const[totalKg, setTotalKg] = useState(0);
-    const[totalTransactions, setTotalTransactions] = useState(0);
+class Impact extends React.Component {
 
-    useEffect(() => {
-      if (props.isConnected){
-        handleCalculateEmissions();
-      } else {
-        alert('connect metamask!');
-      }
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = {
+      totalGas: 0,
+      totalKg: 0,
+      totalTransactions: 0
+    }; 
 
-      return () => {
-        console.log('cleaned up impact');
-      };
-    }, []);
-
-
-    const handleCalculateEmissions = async () => {
-        setTotalGas(undefined);
-        setTotalKg(undefined);
-        setTotalTransactions(undefined);
-
-        var gas = 0;
-        var co2 = 0;
-        var transactions = 0;
-        var typeTransaction = ['eth', 'erc20', 'erc721'];
-        var apiKey;
-        const address = props.account;
-        const callableReturnMessage = firebase.functions().httpsCallable('getEtherscanKey');
-    
-        await callableReturnMessage().then((result) => {
-            apiKey = result.data.etherscanKey;
-        }).catch((error) => {
-          console.log(`error: ${JSON.stringify(error)}`);
-        });
-
-        for (var i = 0; i < 3; i++) {
-            const emissions = await calculateAddressEmissions({
-                transactionType: typeTransaction[i],
-                address: address,
-                etherscanAPIKey: apiKey,
-            });
-            
-            gas += emissions['gasUsed'];
-            co2 += emissions['kgCO2'];
-            transactions += emissions['transactionsCount'];
-        }
-        
-        setTotalGas(gas);
-        setTotalKg(co2);
-        setTotalTransactions(transactions);
+    if (props.isConnected){
+      this.handleCalculateEmissions(props.account);
+    } else {
+      alert('connect metamask!');
     }
+  }
+
+  async handleCalculateEmissions(account) {
+    var gas = 0;
+    var co2 = 0;
+    var transactions = 0;
+    var typeTransaction = ['eth', 'erc20', 'erc721'];
+    var apiKey;
+    const address = account;
+    const callableReturnMessage = firebase.functions().httpsCallable('getEtherscanKey');
+
+    await callableReturnMessage().then((result) => {
+        apiKey = result.data.etherscanKey;
+    }).catch((error) => {
+      console.log(`error: ${JSON.stringify(error)}`);
+    });
+
+    for (var i = 0; i < 3; i++) {
+        const emissions = await calculateAddressEmissions({
+            transactionType: typeTransaction[i],
+            address: address,
+            etherscanAPIKey: apiKey,
+        });
+        
+        gas += emissions['gasUsed'];
+        co2 += emissions['kgCO2'];
+        transactions += emissions['transactionsCount'];
+    }
+    
+    this.setState({
+      totalGas: gas,
+      totalKg: co2,
+      totalTransactions: transactions
+    });
+  }
   
-  return (
-    <div className="Impact">
-      <div className = 'impactContainer'>
-        <div className = 'dashboard'>
-          <div className = 'dashboardHeader'>
-            <p className = 'dashboardTitle'>IMPACT DASHBOARD</p>
-            <p className = 'carbonFootprint'>CARBON FOOTPRINT ⓘ</p>
-            <p className = 'carbonFootprintValue'><CountUp className = 'carbonFootprintValue' end = {totalKg} duration = {1} separator ={','}/> kg CO2</p>
-            
-          </div>
-          <p className = 'emissionsTitle'>EMISSIONS</p>
-          <div className = 'dashboardContent'>
-            <div className = 'dashboardLeft'>
-              <p className = 'contentHeader'>Transactions</p>
-              <CountUp className = 'emissionsValue' end = {totalTransactions} duration = {1} separator ={','}/>
+  render() {
+    return (
+      <div className="Impact">
+        <div className = 'impactContainer'>
+          <div className = 'dashboard'>
+            <div className = 'dashboardHeader'>
+              <p className = 'dashboardTitle'>IMPACT DASHBOARD</p>
+              <p className = 'carbonFootprint'>CARBON FOOTPRINT ⓘ</p>
+              <p className = 'carbonFootprintValue'><CountUp className = 'carbonFootprintValue' end = {this.state.totalKg} duration = {1} separator ={','}/> kg CO2</p>
+              
             </div>
-            <div className = 'dashboardMiddle'>
-              <p className = 'contentHeader'>Gas spent (wei)</p>
-              <CountUp className = 'emissionsValue' end = {totalGas} duration = {1} separator ={','}/>
+            <p className = 'emissionsTitle'>EMISSIONS</p>
+            <div className = 'dashboardContent'>
+              <div className = 'dashboardLeft'>
+                <p className = 'contentHeader'>Transactions</p>
+                <CountUp className = 'emissionsValue' end = {this.state.totalTransactions} duration = {1} separator ={','}/>
+              </div>
+              <div className = 'dashboardMiddle'>
+                <p className = 'contentHeader'>Gas spent (wei)</p>
+                <CountUp className = 'emissionsValue' end = {this.state.totalGas} duration = {1} separator ={','}/>
+              </div>
+              <div className = 'dashboardRight'>
+                <p className = 'contentHeader'>CO2 produced (kg)</p>
+                <CountUp className = 'emissionsValue' end = {this.state.totalKg} duration = {1} separator ={','}/>
+              </div>
             </div>
-            <div className = 'dashboardRight'>
-              <p className = 'contentHeader'>CO2 produced (kg)</p>
-              <CountUp className = 'emissionsValue' end = {totalKg} duration = {1} separator ={','}/>
+            <p className = 'impactTitle'>IMPACT</p>
+            <div className = 'dashboardContent'>
+              <div className = 'dashboardLeft'>
+                <p className = 'contentHeader'>NFTrees</p>
+                <CountUp className = 'impactValue' end = {this.state.totalTransactions} duration = {1} separator ={','}/>
+              </div>
+              <div className = 'dashboardRight'>
+                <p className = 'contentHeader'>Trees planted</p>
+                <CountUp className = 'impactValue' end = {this.state.totalTransactions} duration = {1} separator ={','}/>
+              </div>
+              <div className = 'dashboardMiddle'>
+                <p className = 'contentHeader'>CO2 offset</p>
+                <CountUp className = 'impactValue' end = {this.state.totalTransactions} duration = {1} separator ={','}/>
+              </div>
             </div>
-          </div>
-          <p className = 'impactTitle'>IMPACT</p>
-          <div className = 'dashboardContent'>
-            <div className = 'dashboardLeft'>
-              <p className = 'contentHeader'>NFTrees</p>
-              <CountUp className = 'impactValue' end = {totalTransactions} duration = {1} separator ={','}/>
+            <div className = 'dashboardSummary'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. 
+              Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus 
+              nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
             </div>
-            <div className = 'dashboardRight'>
-              <p className = 'contentHeader'>Trees planted</p>
-              <CountUp className = 'impactValue' end = {totalTransactions} duration = {1} separator ={','}/>
-            </div>
-            <div className = 'dashboardMiddle'>
-              <p className = 'contentHeader'>CO2 offset</p>
-              <CountUp className = 'impactValue' end = {totalTransactions} duration = {1} separator ={','}/>
-            </div>
-          </div>
-          <div className = 'dashboardSummary'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. 
-            Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus 
-            nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Impact;
--- nftrees-app/src/components/Plant.js
+++ nftrees-app/src/components/Plant.js
@@ -10,21 +10,51 @@ import usdt from '../assets/usdt_logo.png';
 
 import Web3 from 'web3';
 
-function Plant(props) {
-    const[level, setLevel] = useState(1);
-    const[coinIndex, setcoinIndex] = useState(0);
-    const[totalCost, setTotalCost] = useState(10);
-    const[coinMenuOpen, setCoinMenuOpen] = useState(false);
-    const[isApproved, setIsApproved] = useState(false);
-    const[test, setTest] = useState();
+class Plant extends React.Component {
+    constructor (props) {
+        super(props);
+        // Don't call this.setState() here!
+        this.state = {
+            level: 1,
+            coinIndex: 0,
+            totalCost: 10,
+            coinMenuOpen: false,
+            isApproved: false,
+            test: 0
+        }; 
 
-    const coins = [
-        'DAI',
-        'USDC',
-        'USDT'
-    ];
+        this.coins = [
+            'DAI',
+            'USDC',
+            'USDT'
+        ];
 
-    useEffect(() => {
+        this.options = [
+            { value: 'DAI', label: <div className = 'currencyOption'><div className = 'currencyLogo'><img src={dai} height="20px" width="20px"/></div><p className = 'currencyText'>DAI</p></div> },
+            { value: 'USDC', label: <div className = 'currencyOption'><div className = 'currencyLogo'><img src={usdc} height="20px" width="20px"/></div><p className = 'currencyText'>USDC</p></div> },
+            { value: 'USDT', label: <div className = 'currencyOption'><div className = 'currencyLogo'><img src={usdt} height="20px" width="20px"/></div><p className = 'currencyText'>USDT</p></div> },
+        ];
+
+        this.defaultOption = this.options[0];
+
+        /*this.addEventListeners = this.addEventListeners.bind(this);
+        this.displayButton = this.displayButton.bind(this);
+        this.displayNFTree = this.displayNFTree.bind(this);
+        this.displayLevel = this.displayLevel.bind(this);
+        this.displayLevelDescription = this.displayLevelDescription.bind(this);
+        this.displayTotal = this.displayTotal.bind(this);
+        this.incLevel = this.incLevel.bind(this);
+        this.decLevel = this.decLevel.bind(this);
+        this.changeCurrency = this.changeCurrency.bind(this);
+        this.checkApproval = this.checkApproval.bind(this);
+        this.approve = this.approve.bind(this);
+        this.buyNFTree = this.buyNFTree.bind(this);*/
+
+        this.checkApproval();
+        this.addEventListeners();
+    }
+
+    /*useEffect(() => {
         const defaultOption = options[0];
         checkApproval();
         addEventListeners();
@@ -32,53 +62,55 @@ function Plant(props) {
         return () => {
             console.log('cleaned up plant');
         };
-    },[totalCost, test]);
+    },[totalCost, test]);*/
 
-    const addEventListeners = async () => {
-        props.DAIContract.events.allEvents()
+    addEventListeners = async () => {
+        this.props.DAIContract.events.allEvents()
         .on('data', (event) => {
-        console.log(event);
-        setTest(event);
+            console.log(event);
+            this.setState({
+                test: event
+            }); 
         })
         .on('error', console.error);
     }
 
-    function displayButton() {
-        if(isApproved === false) {
+    displayButton = () => {
+        if(this.state.isApproved === false) {
             return(
-                <button className = 'plantButton' onClick = {approve}>
-                    <p> APPROVE {totalCost} {coins[coinIndex]}</p> 
+                <button className = 'plantButton' onClick = {this.approve}>
+                    <p> APPROVE {this.state.totalCost} {this.coins[this.state.coinIndex]}</p> 
                 </button>
             )
         } else {
             return(
-                <button className = 'plantButton' onClick = {buyNFTree}>
+                <button className = 'plantButton' onClick = {this.buyNFTree}>
                     <p> PLANT </p> 
                 </button>
             )
         }
     }
 
-    function displayNFTree() {
+    displayNFTree = () => {
         return(
             <img src = {card} height = {560} width = {400}/>
         )
     }
 
-    function displayLevel() {
+    displayLevel = () => {
         return(
-            'NFTree level ' + String(level)
+            'NFTree level ' + String(this.state.level)
         )
     }
 
-    function displayLevelDescription(){
-        if(level === 1){
+    displayLevelDescription = () => {
+        if(this.state.level === 1){
             return('1 tonne CO2 offset + 1 tree planted');
         }
-        else if (level === 2){
+        else if (this.state.level === 2){
             return('10 tonnes CO2 offset + 10 trees planted');
         }
-        else if (level === 3){
+        else if (this.state.level === 3){
             return('100 tonnes CO2 offset + 100 trees planted');
         }
         else{
@@ -86,123 +118,134 @@ function Plant(props) {
         }
     }
 
-    function displayTotal() {
+    displayTotal = () => {
         var coin;
-        if (coinIndex === 0){
+        if (this.state.coinIndex === 0){
             coin = 'DAI';
         }
-        else if (coinIndex === 1) {
+        else if (this.state.coinIndex === 1) {
             coin = 'USDC';
         }
-        else if (coinIndex === 2) {
+        else if (this.state.coinIndex === 2) {
             coin = 'USDT';
         }
         return(
-            '$' + String(totalCost) + ' ' + coin
+            '$' + String(this.state.totalCost) + ' ' + coin
         )
     }
 
-    function incLevel() {
-        if(level < 4){
-            setLevel(level + 1);
-            setTotalCost(totalCost * 10);
+    incLevel = () => {
+        if(this.state.level < 4){
+            this.setState({
+                level: this.state.level + 1,
+                totalCost: this.state.totalCost * 10,
+            });
+            console.log(this.state);
         }
     }
 
-    function decLevel() {
-        if(level > 1){
-            setLevel(level - 1);
-            setTotalCost(totalCost / 10);
+    decLevel = () => {
+        if(this.state.level > 1){
+            this.setState({
+                level: this.state.level - 1,
+                totalCost: this.state.totalCost / 10,
+            }); 
         }
     }
 
-    const checkApproval = async () => {
-        let allowance = await props.getAllowance(coins[coinIndex]);
-        if(allowance < totalCost * (10**18)){
-            setIsApproved(false);
+    checkApproval = async () => {
+        let allowance = await this.props.getAllowance(this.coins[this.state.coinIndex]);
+        if(allowance < this.state.totalCost * (10**18)){
+            this.setState({
+                isApproved: false
+            }); 
         }
-        else{
-            setIsApproved(true);
+        else {
+            this.setState({
+                isApproved: true
+            }); 
         }
     }
 
-    const approve = async () => {
-        if(props.isConnected){
-            props.approve(totalCost, coins[coinIndex]);
+    approve = async () => {
+        if(this.props.isConnected){
+            this.props.approve(this.state.totalCost, this.coins[this.state.coinIndex]);
         } else {
             alert("connect metamask!");
         }
     }
 
-    const buyNFTree = async () => {
-        let numCredits = totalCost / 10;
-        if(props.isConnected){
-            props.buyNFTree(numCredits, totalCost, coins[coinIndex]);
+    buyNFTree = async () => {
+        let numCredits = this.state.totalCost / 10;
+        if(this.props.isConnected){
+            this.props.buyNFTree(this.state.numCredits, this.state.totalCost, this.coins[this.state.coinIndex]);
         } else {
             alert("connect metamask!");
         }
     }
 
-    const options = [
-        { value: 'DAI', label: <div className = 'currencyOption'><div className = 'currencyLogo'><img src={dai} height="20px" width="20px"/></div><p className = 'currencyText'>DAI</p></div> },
-        { value: 'USDC', label: <div className = 'currencyOption'><div className = 'currencyLogo'><img src={usdc} height="20px" width="20px"/></div><p className = 'currencyText'>USDC</p></div> },
-        { value: 'USDT', label: <div className = 'currencyOption'><div className = 'currencyLogo'><img src={usdt} height="20px" width="20px"/></div><p className = 'currencyText'>USDT</p></div> },
-    ];
-    // const defaultOption = options[0];
-
-    function changeCurrency(props) {
-        var currency = props.value;
+    changeCurrency = (event) => {
+        console.log(event.value);
+        var currency = event.value;
         if (currency === 'DAI'){
-            setcoinIndex(0);
+            this.setState({
+                coinIndex: 0
+            }); 
         }
         else if (currency === 'USDC') {
-            setcoinIndex(1);
+            this.setState({
+                coinIndex: 1
+            }); 
         }
         else if (currency === 'USDT') {
-            setcoinIndex(2);
+            this.setState({
+                coinIndex: 2
+            }); 
         }
     }
 
-  return (
-    <div className = "Plant">
-        <div className = 'plantContainer'>
-            <div className = 'plantGrid'>
-                <div className = 'plantLeft'>
-                    <p className = 'plantTitle'>PLANT</p>
+    render() {
+        return (
+            <div className = "Plant">
+                <div className = 'plantContainer'>
+                    <div className = 'plantGrid'>
+                        <div className = 'plantLeft'>
+                            <p className = 'plantTitle'>PLANT</p>
 
-                    <div className = 'levelSelector'>
-                        <div className = 'selector'> 
-                            <button className = 'selectorButton' onClick = {decLevel}> - </button>
-                            <button className = 'selectorButton' onClick = {incLevel}> + </button>
+                            <div className = 'levelSelector'>
+                                <div className = 'selector'> 
+                                    <button className = 'selectorButton' onClick = {this.decLevel}> - </button>
+                                    <button className = 'selectorButton' onClick = {this.incLevel}> + </button>
+                                </div>
+
+                                <div className = 'level'> 
+                                    <p className = 'levelText'> {this.displayLevel()} </p>
+                                </div>
+                            </div>
+
+                            <div className = 'levelDescription'>
+                                <p className = 'description'>{this.displayLevelDescription()}</p>
+                            </div>
+
+                            <div className = 'currencySelector'>
+                                <div className = 'currency'> 
+                                    <Dropdown className = 'currencyDropdown' options={this.options} onChange={this.changeCurrency} value={this.options[this.state.coinIndex]} placeholder="Select currency"/>
+                                </div>
+
+                                <div className = 'total'> 
+                                    <p className = 'totalText'> {this.displayTotal()} </p>
+                                </div>
+                            </div>
+
+                            {this.displayButton()}
                         </div>
 
-                        <div className = 'level'> 
-                            <p className = 'levelText'> {displayLevel()} </p>
-                        </div>
+                        <div className = 'plantRight'>{this.displayNFTree()}</div>
                     </div>
-
-                    <div className = 'levelDescription'>
-                        <p className = 'description'>{displayLevelDescription()}</p>
-                    </div>
-
-                    <div className = 'currencySelector'>
-                        <div className = 'currency'> 
-                            <Dropdown className = 'currencyDropdown' options={options} onChange={changeCurrency} value={options[coinIndex]} placeholder="Select currency"/>
-                        </div>
-
-                        <div className = 'total'> 
-                            <p className = 'totalText'> {displayTotal()} </p>
-                        </div>
-                    </div>
-
-                    {displayButton()}
                 </div>
-
-                <div className = 'plantRight'>{displayNFTree()}</div>
             </div>
-        </div>
-    </div>
-  );
+        );
+    }   
 }
 
 export default Plant;
